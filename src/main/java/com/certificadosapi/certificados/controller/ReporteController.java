0f7e254afc07b773d2ebf42a904e9693fe4e1176
Marlon Morales <marlonfadim@hotmail.com>
2025-11-05 17:25:25 -0500

    Eliminar imports inutilizados

--- src/main/java/com/certificadosapi/certificados/controller/ReporteController.java
+++ src/main/java/com/certificadosapi/certificados/controller/ReporteController.java
@@ -1,40 +1,10 @@
 package com.certificadosapi.certificados.controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.NTCredentials;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.util.EntityUtils;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.impl.auth.NTLMSchemeFactory;
-import org.apache.http.auth.AuthSchemeProvider;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import java.sql.*;
-
-import org.apache.pdfbox.multipdf.PDFMergerUtility;
-
 import com.certificadosapi.certificados.service.ReporteService;
-import com.certificadosapi.certificados.util.ServidorUtil;
-import com.sun.jna.platform.win32.Advapi32Util;
-import com.sun.jna.platform.win32.WinReg;
 
 import org.springframework.http.*;
 
@@ -69,7 +39,7 @@ public class ReporteController {
         return ResponseEntity.ok(documentos);
     }
 
-    
+
     @GetMapping("/facturas/existe")
     public ResponseEntity<?> existeFactura(
             @RequestParam String idAdmision,
